f76e239c8d88242c34fd60794739a71442b04978
Pavel <grigorov_ps@mail.ru>
2023-10-01 13:32:52 +0300

    Создали программу Example001, которая на вход принимает два числа // и проверяет, является ли первое число квадратом второго.

---
+++ Example001_mathematical_methods/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая на вход принимает два числа 
+// и проверяет, является ли первое число квадратом второго.
+
+using System.Collections.Specialized;
+
+Console.WriteLine("Введите первое число : ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число : ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if(numA == numB * numB){
+    Console.WriteLine("Условие выполнено.");
+}
+else{
+    Console.WriteLine("Не верно, попробуйте ещё раз.");
+}
