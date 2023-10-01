df11cfba3357a450708a6ffb721c3ea611563005
Pavel <grigorov_ps@mail.ru>
2023-10-01 23:02:05 +0300

    Создали проект Example004 Который определяет последнюю цифру в трёхзначном числе

---
+++ Example004/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите трёхзначное число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 0)
+{
+    number = -1 * number;
+}
+
+if (number >= 100 && number <= 999)
+{
+    int lastNumber = number % 10;
+    Console.WriteLine($"{number} -> {lastNumber}");
+}
+else
+{
+    Console.WriteLine($"{number} -> Не трёхзначное число! ");
+}
