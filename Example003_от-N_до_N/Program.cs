8cceaa8e4e7b657f7677bd2efd0e124e6685d5cb
Pavel <grigorov_ps@mail.ru>
2023-10-01 22:06:44 +0300

    Создали программу Exemple003 которая на вход принимает число (N), // а на выходе показывает все целые числа в промежутке от -N до N.

---
+++ Example003_от-N_до_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -1 * n;
+
+while (count <= n)
+{
+    Console.Write($"{count}, ");
+    count = count +1;
+}
