c3466806240b2363488f38cca80e24719efe307b
Pavel <grigorov_ps@mail.ru>
2023-10-02 10:59:01 +0300

    Добавили коментарии к проекту Example004

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,14 +1,17 @@
-﻿Console.WriteLine("Введите трёхзначное число : ");
+﻿// Написать программу, которая на вход принимает трехзначное число,
+// а на выходе показывает его последнюю цифру.
+
+Console.WriteLine("Введите трёхзначное число : ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if(number < 0)
+if(number < 0)   // Проверка на отрицательное значение
 {
     number = -1 * number;
 }
 
-if (number >= 100 && number <= 999)
+if (number >= 100 && number <= 999)    // Условие что число трехзначное
 {
-    int lastNumber = number % 10;
+    int lastNumber = number % 10;     // %-остаток от деления. %10 - всегда последнее число.
     Console.WriteLine($"{number} -> {lastNumber}");
 }
 else
